ffdc4ae479a09513b1635b0fafcc9c813a32051a
rahmad firdaus <163318781+rhmdfrds1212@users.noreply.github.com>
2024-10-25 17:07:46 +0700

    update 3

--- LATIHAN_UTS/app.js
+++ LATIHAN_UTS/app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 //LOAD MONGODB DB CONNECTION
 var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
+var UsersRouter = require('./app_server/routes/index');
+var ProdukRouter = require('./app_server/routes/produk')
 
 var app = express();
 
@@ -20,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', UsersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

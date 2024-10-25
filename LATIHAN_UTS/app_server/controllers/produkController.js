967b099d6fb71f9edb117f25636cd060dddd50e2
rahmad firdaus <163318781+rhmdfrds1212@users.noreply.github.com>
2024-10-25 16:59:08 +0700

    update 2

--- LATIHAN_UTS/app_server/controllers/produkController.js
+++ LATIHAN_UTS/app_server/controllers/produkController.js
@@ -1,11 +1,37 @@
-const Product = require('../models/produk');
-
-const index = (req, res) => {
- 	res.render('index', { title: 'Express' }); 
-}; 
-
-const produk = (req,res) => {
-	res.render('produk', { title:
-		'Express'});
+const Produk = require('../models/produk');
+	
+exports.index = async (req, res) => {
+  try {
+    const produk = await Produk.find();
+    res.status(200).json({
+      code: 200,
+      message: "Success",
+      data: produk
+    });
+  } catch (error) {
+    res.status(400).json({
+      code: 400,
+      message: "Failed"
+    });
+  }
 };
-module.exports = { index }; 
+
+// Insert new produk
+exports.insert = async (req, res) => {
+  const { nama, deskripsi, harga, stok, kategori_id } = req.body;
+  
+  const produk = new Produk({ nama, deskripsi, harga, stok, kategori_id });
+
+  try {
+    await produk.save();
+    res.status(200).json({
+      code: 200,
+      message: "Success"
+    });
+  } catch (error) {
+    res.status(400).json({
+      code: 400,
+      message: "Failed"
+    });
+  }
+};
